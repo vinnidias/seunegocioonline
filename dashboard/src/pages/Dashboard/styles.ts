7eaef3911adbb836f60658f7ef867b635ff4659f
vinnidias <vinicius.3991@gmail.com>
2022-07-28 15:18:18 -0300

    initial theme config

---
+++ dashboard/src/pages/Dashboard/styles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import { Theme } from "../../styles/Theme";
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  background-color: ${Theme.colors.mainBg};
+  min-height: 100vh;
+`;
---
+++ dashboard/src/styles/Theme.ts
@@ -0,0 +1,10 @@
+export const Theme = {
+  colors: {
+      mainBg: '#f5fafa',
+      secondaryBg: '#3b9ef5'
+  },
+  fonts: {
+      
+  },
+  
+};

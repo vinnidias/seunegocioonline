bc080ba1be3b801bf566dbc183c1b3b0ecba4134
vinnidias <vinicius.3991@gmail.com>
2022-08-04 23:39:13 -0300

    passing private routes

--- dashboard/src/Router/index.tsx
+++ dashboard/src/Router/index.tsx
@@ -4,40 +4,37 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Dashboard } from "../pages/Dashboard";
 import { Login } from "../pages/Login";
-import { MenuProvider } from "../contexts/MenuContexts";
 import { PrivateRoute } from "../components/Routes/PrivateRoute";
+import { MenuProvider } from "../contexts/MenuContexts";
+import { AuthProvider } from "../contexts/AuthContext";
 
 export const Router: React.FC = () => {
   return (
     <BrowserRouter>
-      <MenuProvider>
-        <Routes>
-        <Route
-            path="/login"
-            element={
-             <Login/>
-            }
-          />
-          <Route
-            path="/"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-        <Routes>
-          <Route
-            path="/dashboard"
-            element={
-              <PrivateRoute>
-                <Dashboard />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </MenuProvider>
+      <AuthProvider>
+        <MenuProvider>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+
+            <Route
+              path="/dashboard"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </MenuProvider>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
